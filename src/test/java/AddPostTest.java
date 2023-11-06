cf1a6a1660437a1a1d1393605e5a9ecc8e361405
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-11-06 13:34:53 +0100

    add post from file

--- src/test/java/AddPostTest.java
+++ src/test/java/AddPostTest.java
@@ -1,6 +1,8 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class AddPostTest {
@@ -13,6 +15,15 @@ public class AddPostTest {
                 "}";
 
         given().log().all().contentType(ContentType.JSON).body(newPostBody)
+                .when().post("http://localhost:3000/posts")
+                .then().log().all();
+    }
+
+    @Test
+    public void addPostFromFile(){
+        File newPost = new File("src/test/resources/post.json");
+
+        given().log().all().contentType(ContentType.JSON).body(newPost)
                 .when().post("http://localhost:3000/posts")
                 .then().log().all();
     }
