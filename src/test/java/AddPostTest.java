2047720c8eac8a9c55609d4a4caf82e038a059a3
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-11-06 13:43:15 +0100

    add post from map add Jackson Databind

--- src/test/java/AddPostTest.java
+++ src/test/java/AddPostTest.java
@@ -2,6 +2,8 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,6 +24,17 @@ public class AddPostTest {
     @Test
     public void addPostFromFile(){
         File newPost = new File("src/test/resources/post.json");
+
+        given().log().all().contentType(ContentType.JSON).body(newPost)
+                .when().post("http://localhost:3000/posts")
+                .then().log().all();
+    }
+
+    @Test
+    public void addPostMap(){
+        Map<String, Object> newPost = new HashMap<>();
+        newPost.put("title", "TitleFromMap");
+        newPost.put("author", "Jakub The Third");
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
                 .when().post("http://localhost:3000/posts")
