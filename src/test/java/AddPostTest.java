999a28240be3f32b2b999e780b08fc5badaf3209
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-11-06 13:48:56 +0100

    add post from Object add model package

--- src/test/java/AddPostTest.java
+++ src/test/java/AddPostTest.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import model.Post;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -10,7 +11,7 @@ import static io.restassured.RestAssured.given;
 public class AddPostTest {
 
     @Test
-    public void addPost(){
+    public void addPost() {
         String newPostBody = "{\n" +
                 "    \"title\": \"addPostRestAssuredTest\",\n" +
                 "    \"author\": \"Jakub\"\n" +
@@ -22,7 +23,7 @@ public class AddPostTest {
     }
 
     @Test
-    public void addPostFromFile(){
+    public void addPostFromFile() {
         File newPost = new File("src/test/resources/post.json");
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
@@ -31,10 +32,21 @@ public class AddPostTest {
     }
 
     @Test
-    public void addPostMap(){
+    public void addPostMap() {
         Map<String, Object> newPost = new HashMap<>();
-        newPost.put("title", "TitleFromMap");
+        newPost.put("title", "Title From Map");
         newPost.put("author", "Jakub The Third");
+
+        given().log().all().contentType(ContentType.JSON).body(newPost)
+                .when().post("http://localhost:3000/posts")
+                .then().log().all();
+    }
+
+    @Test
+    public void addPostObject() {
+        Post newPost = new Post();
+        newPost.setAuthor("Jakub The Fourth");
+        newPost.setTitle("Title From Object");
 
         given().log().all().contentType(ContentType.JSON).body(newPost)
                 .when().post("http://localhost:3000/posts")
