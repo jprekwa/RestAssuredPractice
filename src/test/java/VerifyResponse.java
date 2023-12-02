d4dfa25e01953a0176adebda5a8a2b6f0a285eb7
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-12-02 16:45:35 +0100

    add verifying response body by mapping it to the class object

--- src/test/java/VerifyResponse.java
+++ src/test/java/VerifyResponse.java
@@ -1,4 +1,6 @@
+import model.Post;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,24 +17,40 @@ public class VerifyResponse {
                 "}";
 
         given().log().all()
-                .when().get("http://localhost:3000/posts/{postId}", 1).
-                then().log().all().body(Matchers.equalTo(expected));
+                .when().get("http://localhost:3000/posts/{postId}", 1)
+                .then().log().all().body(Matchers.equalTo(expected));
     }
 
     //verifying part of response body
     @Test
     public void getPostContains() {
         given().log().all()
-                .when().get("http://localhost:3000/posts/{postId}", 1).
-                then().log().all().body(Matchers.containsStringIgnoringCase("jakub"));
+                .when().get("http://localhost:3000/posts/{postId}", 1)
+                .then().log().all().body(Matchers.containsStringIgnoringCase("jakub"));
     }
 
     //verifying two field in response body
     @Test
     public void checkSpecificField() {
         given().log().all()
-                .when().get("http://localhost:3000/posts/{postId}", 1).
-                then().log().all().body("title", Matchers.equalTo("updatePost"))
+                .when().get("http://localhost:3000/posts/{postId}", 1)
+                .then().log().all().body("title", Matchers.equalTo("updatePost"))
                 .and().body("author", Matchers.equalTo("Jakub The First"));
+    }
+
+    //verifying response body using response body as an object
+    @Test
+    public void getPostObject() {
+        Integer id = 1;
+
+        Post newPost = given().log().all()
+                .when().get("http://localhost:3000/posts/{postId}", id)
+                .then().log().all().body("title", Matchers.equalTo("updatePost"))
+                .and().body("author", Matchers.equalTo("Jakub The First"))
+                .extract().as(Post.class);
+
+        Assert.assertEquals(newPost.getAuthor(), "Jakub The First");
+        Assert.assertEquals(newPost.getTitle(), "updatePost");
+        Assert.assertEquals(newPost.getId(), id);
     }
 }
--- src/test/java/model/Post.java
+++ src/test/java/model/Post.java
@@ -5,8 +5,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Post {
 
+    private Integer id;
     private String title;
     private String author;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
