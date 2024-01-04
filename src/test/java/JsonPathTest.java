1ffc0a730497b38132d8aeced41df5175f4ff830
Jakub Prekwa <jakub.prekwa@gmail.com>
2024-01-04 11:57:52 +0100

    JSONPath's basics - getting specific fields within body using JSONPath class

--- src/test/java/JsonPathTest.java
+++ src/test/java/JsonPathTest.java
@@ -2,12 +2,32 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class JsonPathTest {
 
+    //getting specific fields within body using JSONPath class
     @Test
     public void checkSpecificFieldJsonPath() {
         Response response = RestAssured.get("http://localhost:3000/posts/{postId}", 1);
 
         System.out.println(response.asString());
+
+        String secretString = response.path("secretString");
+        List<Integer> winningNumbers = response.path("winning-numbers");
+//      String firstWinnerName = response.path("winners.name[0]");
+        String firstWinnerName = response.path("winners[0].name");
+        String secondWinnerName = response.path("winners[1].name");
+        String lastWinnerName = response.path("winners[-1].name");
+
+        //get List with all names of winners
+        List<String> winnerNames = response.path("winners.name");
+
+        //get all information about the first winner
+        Map<String,?> winner = response.path("winners[0]");
+
+        //get all information about all the winners
+        List<Map<String, ?>> winners = response.path("winners");
     }
 }
