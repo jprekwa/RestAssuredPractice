9bc2473315b73f28c82a3115ff4a27105d1902d0
Jakub Prekwa <jakub.prekwa@gmail.com>
2024-01-04 12:25:38 +0100

    JSONPath's basics - getting specific fields within body using JSONPath class - continuation

--- src/test/java/JsonPathTest.java
+++ src/test/java/JsonPathTest.java
@@ -14,20 +14,35 @@ public class JsonPathTest {
 
         System.out.println(response.asString());
 
+        //getting specific fields
         String secretString = response.path("secretString");
         List<Integer> winningNumbers = response.path("winning-numbers");
-//      String firstWinnerName = response.path("winners.name[0]");
+//        String firstWinnerName = response.path("winners.name[0]");
         String firstWinnerName = response.path("winners[0].name");
         String secondWinnerName = response.path("winners[1].name");
         String lastWinnerName = response.path("winners[-1].name");
 
-        //get List with all names of winners
+        //getting List with all names of winners
         List<String> winnerNames = response.path("winners.name");
 
-        //get all information about the first winner
-        Map<String,?> winner = response.path("winners[0]");
+        //getting all information about the first winner
+        Map<String, ?> winner = response.path("winners[0]");
 
-        //get all information about all the winners
+        //getting all information about all the winners
         List<Map<String, ?>> winners = response.path("winners");
+
+        //getting all information about the winner with specific name
+        Map<String, ?> winnerInfo = response.path("winners.find{it.name='Andrew'}");
+        Integer winnerInfoId = response.path("winners.find{it.name='Andrew'}.winnerId");
+
+        //getting the highest and lowest values from the table
+        Integer maxNumber = response.path("winning-numbers.max()");
+        Integer minNumber = response.path("winning-numbers.min()");
+
+        //getting winner with the highest ID
+        Map<String, ?> winnerMaxId = response.path("winners.max{it.winnerId}");
+
+        //getting sum of two numbers
+        Integer moneySum = response.path("winners.collect{it.money}.sum()");
     }
 }
